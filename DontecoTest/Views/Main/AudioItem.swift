4ac4bfcc01cf1de11d74b32166f80f652f27b168
Artem Lastovich <art.lastovich@gmail.com>
2022-06-08 00:16:33 +0300

    fixed button label width

--- DontecoTest/Views/Main/AudioItem.swift
+++ DontecoTest/Views/Main/AudioItem.swift
@@ -38,11 +38,13 @@ struct AudioItem: View {
                     showImporter(itemPlace)
                 } label: {
                     Image(systemName: fileName == nil ? "plus" : "arrow.triangle.2.circlepath")
+                        .frame(width: 20, height: 20)
                         .padding()
                         .background(Color.white)
                         .foregroundColor(.black)
                         .clipShape(RoundedRectangle(cornerRadius: 15))
                         .shadow(color: .black.opacity(0.1), radius: 20)
+                        .aspectRatio(1, contentMode: .fit)
                 }
                 .buttonStyle(ScaledButton())
                 .opacity(audioService.isPlaying ? 0 : 1)
